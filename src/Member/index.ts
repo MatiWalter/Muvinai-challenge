bc9639477d34d12364c69f93f9a497112175df5b
Matias Walter <matiwalter97@hotmail.com>
2021-11-12 19:45:35 -0300

    add exports

---
+++ src/Member/index.ts
@@ -0,0 +1,4 @@
+export type { IMember, TActivePlan } from './types';
+export { MemberAvatar } from './MemberAvatar';
+export { MemberProfile } from './MemberProfile';
+export { MemberCard } from './MemberCard';
