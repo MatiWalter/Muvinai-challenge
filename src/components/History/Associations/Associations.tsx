ad60dbab279ce48fe0458b87217b30ccc0c646f1
Matias Walter <matiwalter97@hotmail.com>
2021-11-16 10:35:03 -0300

    Fix/styles: Associations

--- src/components/History/Associations/Associations.tsx
+++ src/components/History/Associations/Associations.tsx
@@ -1,14 +1,11 @@
 import { Container, Table, Title } from '@mantine/core';
-import { useMantineColorScheme, useMantineTheme } from '@mantine/styles';
 import Scrollbars from 'react-custom-scrollbars-2';
 
-import { associations } from '../../data/associations';
+import { associations } from '~/data/associations';
+
+import useStyles from './Associations.styles';
 
 export const Associations = () => {
-
-  const { colors, white } = useMantineTheme();
-  const { colorScheme } = useMantineColorScheme();
-  const dark = colorScheme === 'dark';
 
   const rows = associations.map(association => {
     return (
@@ -19,18 +16,10 @@ export const Associations = () => {
     );
   });
 
+  const { classes: { container } } = useStyles();
+
   return (
-    <Container
-      mt='xl'
-      sx={{
-        backgroundColor: dark ? colors.dark[7] : white,
-        boxShadow: dark ? 'none' : 'rgba(149, 157, 165, 0.2) 0px 8px 24px',
-        borderRadius: 10,
-        padding: 25,
-        maxWidth: '700px',
-        height: '500px'
-      }}
-    >
+    <Container className={container}>
       <Title align='center' order={2}>Asociaciones pasadas</Title>
       <Scrollbars>
         <Table highlightOnHover mt={40}>
